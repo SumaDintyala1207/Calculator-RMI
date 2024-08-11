ecb8f7b82581ceef47e5f51ac28e12617997bb8c
SumaDintyala1207 <a1950568@adelaide.edu.au>
2024-08-11 19:12:26 +0930

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,7 +1,9 @@
+package rmi_calculator;
+
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
-public interface Calculator  extends Remote{
+public interface Calculator  extends Remote {
     void pushValue(int val) throws RemoteException;
     void pushOperation(String operator) throws RemoteException;
     int pop() throws RemoteException;
